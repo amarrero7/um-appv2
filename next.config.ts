bb1b61f04b128bc1d22bd66865c811ceba31fc69
amarrero7 <alejandromarrero89@gmail.com>
2025-06-17 15:33:53 -0400

    chnages for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'standalone',
-};
+  // Optional: Use this if you deploy under a sub-path
+  // basePath: '/your-base-path',
+  reactStrictMode: true,
+}
 
-export default nextConfig;
+module.exports = nextConfig
